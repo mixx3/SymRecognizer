47f018742aec503fc5f065ba3065bf115b670e32
MikeP <parfenov.ma21@physics.msu.ru>
2023-05-05 21:27:18 +0300

    initial

---
+++ main.py
@@ -0,0 +1,12 @@
+from recognizer import Recognizer, Morphology
+import cv2 as cv
+
+
+def print_hi():
+    img = cv.imread('static/img_1.png', cv.IMREAD_GRAYSCALE)
+    s = Recognizer.recognize(img, Morphology)
+    print(s)
+
+
+if __name__ == '__main__':
+    print_hi()
---
+++ tests.py
@@ -0,0 +1,8 @@
+from recognizer import Recognizer, Morphology, Correlation
+import cv2 as cv
+
+
+if __name__ == "__main__":
+    img = cv.imread('static/img.png', cv.IMREAD_GRAYSCALE)
+    print(Recognizer.recognize(img, Morphology))
+    print(Recognizer.recognize(img, Correlation))
